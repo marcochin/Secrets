9cf3a3f7a99a741ad4faf38f9f502d959799ad87
Marco Chin <marcochin@50-1-86-103.dsl.dynamic.fusionbroadband.com>
2020-02-18 13:35:45 -0800

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+// const encrypt = require("mongoose-encryption"); // level 2 encryption
+// const md5 = require("md5"); // level 3 hashing
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 const app = express();
 
@@ -17,18 +25,36 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "This is my big fat secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
+
+// There was a warning when running passport w/ mongoose. Set this to fix it.
+mongoose.set("useCreateIndex", true);
 
 // To use encryption we have to use mongoose.Schema
 // This simple way of using just a js object for schema works if you're not
 // doing anything fancy with them.
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
+
+userSchema.plugin(passportLocalMongoose); // helps us hash + salt pws, were not using bcrypt
+userSchema.plugin(findOrCreate);
 
 // Our secret string to help encrypt passwords. We put this in our .env file now.
 // We use .env to prevent accidentally exposing sensitive info on github.
@@ -39,13 +65,62 @@ const userSchema = new mongoose.Schema({
 // To do this we have to specify the fields we want to encrypt w/ "encryptedFields"
 // This will encrypt when you call save data and automatically decrypt when getting the data.
 // All of this is done behind the scenes
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy()); // use local strategy to authenitcate username/pw (i dunno..)
+passport.serializeUser(function(user, done) { // Creates fortune cookie w/ a msg
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) { // Opens fortune cookie and see msg.
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Don't use this way as this only works for local strategy, whereas the one on top works with all.
+// passport.serializeUser(User.serializeUser()); // Creates fortune cookie w/ a msg
+// passport.deserializeUser(User.deserializeUser()); // Opens fortune cookie and see msg.
+
+// Google+ is gone so retrieve info from userProfileURL->userInfo
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({
+      googleId: profile.id
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home"); //home.ejs
 });
+
+// Passport will authenitcate using the google strategy as defined above
+// Initiate authenitication on google servers asking for user's profile.
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  }));
+
+// Once user has signed in to google they will be redirected here.
+// Authenitcate them locally and save their login session
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", function(req, res) {
   res.render("login"); //login.ejs
@@ -55,34 +130,85 @@ app.get("/register", function(req, res) {
   res.render("register"); //register.ejs
 });
 
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err){
+app.get("/secrets", function(req, res) {
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
     if(err){
       console.log(err);
     }else{
-      res.render("secrets");
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  })
+});
+
+app.get("/submit", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res) {
+  const secret = req.body.secret;
+
+  // find the user who submitted the userDB
+  // passport will save the user details in the request variable
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+
+    } else {
+      if (foundUser) {
+        foundUser.secret = secret;
+
+        foundUser.save(function() {
+          res.redirect("/secrets")
+        });
+      }
+    }
+  });
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+  // Instead of creating a new user and then saving the user
+  // passport-local-mongoose register method takes care of that for us.
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        // If authentication succeeds and successfully created a cookie
+        // Users can directly go to /secrets page cause they have the loggedin cookie.
+        res.redirect("/secrets");
+      });
     }
   });
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
+  req.login(user, function(err) {
+    if (err) {
       console.log(err);
-    }else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-      }
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        // If authentication succeeds and successfully created a cookie
+        // Users can directly go to /secrets page cause they have the loggedin cookie.
+        res.redirect("/secrets");
+      });
     }
   });
 });
